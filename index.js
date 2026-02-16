073749bc73a5c073e93ed7f9f5f0003a6e09c623
Aadit <aadits2004@gmail.com>
2026-02-16 14:08:01 +0530

    created a canvas and a point on screen

---
+++ index.js
@@ -0,0 +1,27 @@
+const BACKGROUND = "#101010";
+const FOREGROUND = "#50FF50";
+
+console.log(game)
+game.width = 800;
+game.height = 800;
+
+const ctx = game.getContext("2d");
+console.log(ctx);
+
+function clear() {
+   ctx.fillStyle = BACKGROUND;
+   ctx.fillRect(0, 0, game.width, game.height)
+}
+
+function point(x, y) {
+   const s = 20;
+   ctx.fillStyle = FOREGROUND;
+   ctx.fillRect(x, y, s, s);
+}
+
+function screen(p) {
+   // -1...1 => 0...w/h
+}
+
+clear()
+point(100, 100);
